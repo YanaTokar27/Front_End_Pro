ac35dab4a59047555152404796bd430af15ffdd0
yanatokar <yanochka.tokar22@gmail.com>
2023-10-10 16:38:50 +0300

    object

---
+++ Lesson-1/object.js
@@ -0,0 +1,16 @@
+let emptyPbject = {
+
+};
+
+let myName = 'Yana';
+
+console.log(emptyPbject);
+
+let Yanochka = {
+    age: 29,
+    sex: female,
+    name|: myName,
+};
+
+console.log(Yanochka);
+
