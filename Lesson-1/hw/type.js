49427f06f5920f9198d96494ac59b558b9981102
yanatokar <yanochka.tokar22@gmail.com>
2023-10-11 12:09:16 +0300

    hw

--- Lesson-1/hw/type.js
+++ Lesson-1/hw/type.js
@@ -1,23 +1,47 @@
-let name3 = 'Yana';
+console.log('Hello!')
 
-console.log (name3);
-console.log (typeof name3);
-
+const name3 = 'Yana';
 let lastName = 'Tokar';
-
-console.log (lastName);
-console.log (typeof lastName);
-
-let myAge = 29;
-console.log (myAge);
-console.log (typeof myAge);
-
-let isTeacher = false;
+const eyes_color = 'green';
+let weight = 65;
+let my_age = 29;
 let isTester = true;
-let isDeveloper = false;
-
-console.log('isTeacher', isTeacher);
-console.log('isTester', isTester);
-console.log('isDeveloper', isDeveloper);
+let have_panda = false;
+let children = null;
 
 
+console.log (name3, typeof name3);
+console.log (lastName, typeof lastName);
+console.log (eyes_color, typeof eyes_color);
+console.log (weight, typeof weight);
+console.log (my_age, typeof my_age);
+console.log (isTester, typeof isTester);
+console.log (have_panda, typeof have_panda);
+console.log (children, typeof children);
+
+
+
+const yana = {
+    age: 29,
+    name: 'Yanochka',
+    sex: 'female',
+    isMaried: false,
+    isStuadent: isTester,
+    my_voice: 'nice',
+    my_size: 46,
+}
+
+console.log (yana);
+
+
+
+
+
+
+alert ('Вам треба пройти опитувальник');
+
+let infoWindow = prompt ('Який стиль одягу ви полюбляєте?');
+console.log (infoWindow, typeof infoWindow);
+
+let confirmWindow = confirm('Чи можна зберегти вашу інформацію?');
+console.log(confirmWindow, typeof confirmWindow);
