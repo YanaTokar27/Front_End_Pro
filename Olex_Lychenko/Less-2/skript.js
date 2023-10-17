7ee364e0ade5c2f39ce7a40908740b16e263dc1e
yanatokar <yanochka.tokar22@gmail.com>
2023-10-17 17:55:21 +0300

    Lesson-2

--- Olex_Lychenko/Less-2/skript.js
+++ Olex_Lychenko/Less-2/skript.js
@@ -5,14 +5,18 @@ console.log(a);
 
 let inputIn = document.querySelector('.input-in');
 let button = document.querySelector('button');
+let div = document.querySelector('.out');
 
 button.onclick = function () {
     // виконується, коли кнопка буде натиснута
     console.log('Work');
     // console.log(inputIn.value); // те, що введене в input
-    let b = inputIn.value;
+    let b = +inputIn.value; //+ переводит в число!
     console.log(b + 10);
     // console.log(b * 2);
     // console.log(b / 2);
     // console.log(b / 0);
+
+    div.innerHTML = b;
+    inputIn.value = '';
 };
