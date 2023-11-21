b4460dc0844a8a73db8656c166598187c487f02e
yanatokar <yanochka.tokar22@gmail.com>
2023-11-21 17:48:04 +0200

    Less-11

--- Olex_Lychenko/Less-11/script.js
+++ Olex_Lychenko/Less-11/script.js
@@ -15,4 +15,17 @@ toggle.onclick = function () {
 }
 
 //атрибути data
+//getAttribute - прочитать дату
 console.log(one.getAttribute('data'));
+console.log(document.querySelector('link').getAttribute('href'));
+
+//додавання нового атрибуту
+one.setAttribute('data-num', 6);
+
+const task1 = document.querySelector('.task1');
+task1.onclick = function () {
+    this.classList.toggle('five');
+}
+
+const task2 = document.querySelector('.task2');
+task2.setAttribute('data-task2', 7);
