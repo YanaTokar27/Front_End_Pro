4353c0094379ac3874b55bc67285214117fa169d
yanatokar <yanochka.tokar22@gmail.com>
2023-11-22 17:06:44 +0200

    Less-11

--- Olex_Lychenko/Less-11/script.js
+++ Olex_Lychenko/Less-11/script.js
@@ -22,10 +22,32 @@ console.log(document.querySelector('link').getAttribute('href'));
 //додавання нового атрибуту
 one.setAttribute('data-num', 6);
 
+let gas = document.querySelectorAll('.gas');
+for (i = 0; i < gas.length; i++) {
+    gas[i].onclick = function () {
+        let gallons = document.querySelector('.gallons').value;
+        let amont = this.getAttribute('data');
+        console.log(gallons * amont);
+    }
+}
+
+let clothes = document.querySelectorAll('.clothes');
+for (i = 0; i < clothes.length; i++) {
+    clothes[i].onclick = function () {
+        let wear = document.querySelector('.wear').value;
+        let salary = this.getAttribute('data');
+        console.log(wear * salary);
+    }
+}
+
 const task1 = document.querySelector('.task1');
 task1.onclick = function () {
     this.classList.toggle('five');
 }
 
 const task2 = document.querySelector('.task2');
-task2.setAttribute('data-task2', 7);
+task2.onclick = function () {
+    this.classList.toggle('bg-orange');
+}
+
+    / task2.setAttribute('data-task2', 7);
