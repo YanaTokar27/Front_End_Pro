b50a60f64a74c6e175b121707e4c613f419ff576
yanatokar <yanochka.tokar22@gmail.com>
2023-11-23 19:16:47 +0200

    Less-11

--- Olex_Lychenko/Less-11/script.js
+++ Olex_Lychenko/Less-11/script.js
@@ -23,12 +23,16 @@ console.log(document.querySelector('link').getAttribute('href'));
 one.setAttribute('data-num', 6);
 
 let gas = document.querySelectorAll('.gas');
+console.log("gas.length:", gas.length);
+
+let gallons = document.querySelector('.gallons').value;
+const calculateAmountOfGallons = function () {
+    let amont = this.getAttribute('data');
+    console.log(gallons * amont);
+}
+// gas.forEach((g) => g.addEventListener('click', calculateAmountOfGallons));
 for (i = 0; i < gas.length; i++) {
-    gas[i].onclick = function () {
-        let gallons = document.querySelector('.gallons').value;
-        let amont = this.getAttribute('data');
-        console.log(gallons * amont);
-    }
+    gas[i].onclick = calculateAmountOfGallons;
 }
 
 let clothes = document.querySelectorAll('.clothes');
@@ -45,9 +49,23 @@ task1.onclick = function () {
     this.classList.toggle('five');
 }
 
-const task2 = document.querySelector('.task2');
-task2.onclick = function () {
-    this.classList.toggle('bg-orange');
-}
+let task2 = document.createElement('button');
+task2.innerHTML = 'Push';
+task2.classList.add('orange');
+task2.addEventListener('click', () => {
+    task2.classList.add('bg-orange');
+});
 
-    / task2.setAttribute('data-task2', 7);
+document.querySelector('.test').appendChild(task2);
+console.log(task2);
+
+let a = document.createElement('div');
+a.innerHTML = 'Hello!';
+a.classList.add('createDiv');
+
+document.querySelector('.test').appendChild(a);
+
+console.log(a);
+
+// task2.setAttribute('data-task2', 7);
+
