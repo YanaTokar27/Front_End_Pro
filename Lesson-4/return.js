c414cd7fbcee3bed440f32c1c75712f8a99d840c
yanatokar <yanochka.tokar22@gmail.com>
2023-11-24 17:59:35 +0200

    Lesson-4

--- Lesson-4/return.js
+++ Lesson-4/return.js
@@ -3,9 +3,9 @@
 function sum(a, b) {
     let c = a + b;
 
-    if(c > 0) {
-       console.log('dfdfd');
-       return c;
+    if (c > 0) {
+        console.log('dfdfd');
+        return c;
     }
 
 
@@ -18,5 +18,23 @@ sum(2, 2);
 
 let result = sum(3, 3);
 
-console.log('result',result);
+console.log('result', result);
 
+
+
+function sum(a, b = 2) {
+    return a + b;
+}
+
+let res1 = sum(2);
+console.log(res1);
+
+
+function func1() {
+    return function func2() {
+        console.log('Function');
+    }
+}
+
+let res = func1();
+res();
