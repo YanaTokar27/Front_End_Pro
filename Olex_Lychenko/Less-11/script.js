b2dcd9e265878aa24b7bef7293e0ca35f29a1212
yanatokar <yanochka.tokar22@gmail.com>
2023-11-24 17:59:51 +0200

    Less-11

--- Olex_Lychenko/Less-11/script.js
+++ Olex_Lychenko/Less-11/script.js
@@ -22,19 +22,22 @@ console.log(document.querySelector('link').getAttribute('href'));
 //додавання нового атрибуту
 one.setAttribute('data-num', 6);
 
+//example with gas
 let gas = document.querySelectorAll('.gas');
 console.log("gas.length:", gas.length);
 
-let gallons = document.querySelector('.gallons').value;
 const calculateAmountOfGallons = function () {
-    let amont = this.getAttribute('data');
+    let gallons = document.querySelector('.gallons').value;
+    let amont = this.getAttribute('value');
     console.log(gallons * amont);
 }
 // gas.forEach((g) => g.addEventListener('click', calculateAmountOfGallons));
+
 for (i = 0; i < gas.length; i++) {
     gas[i].onclick = calculateAmountOfGallons;
 }
 
+//my example
 let clothes = document.querySelectorAll('.clothes');
 for (i = 0; i < clothes.length; i++) {
     clothes[i].onclick = function () {
@@ -43,29 +46,39 @@ for (i = 0; i < clothes.length; i++) {
         console.log(wear * salary);
     }
 }
-
+//task-1
 const task1 = document.querySelector('.task1');
 task1.onclick = function () {
     this.classList.toggle('five');
 }
 
+//task-2
 let task2 = document.createElement('button');
-task2.innerHTML = 'Push';
-task2.classList.add('orange');
+task2.innerHTML = 'Task2';
 task2.addEventListener('click', () => {
     task2.classList.add('bg-orange');
 });
 
-document.querySelector('.test').appendChild(task2);
+document.querySelector('.test2').appendChild(task2);
 console.log(task2);
 
-let a = document.createElement('div');
-a.innerHTML = 'Hello!';
-a.classList.add('createDiv');
+//task-3
+let a = document.createElement('button');
+a.innerHTML = 'Task3';
+a.addEventListener('click', () => {
+    a.classList.add('createDiv');
+})
 
-document.querySelector('.test').appendChild(a);
-
+document.querySelector('.test3').appendChild(a);
 console.log(a);
 
-// task2.setAttribute('data-task2', 7);
+//task-remove
+let rem = document.querySelector('#task3');
+let b = document.createElement('button');
+b.innerHTML = 'Remove';
+document.querySelector('#task3').appendChild(b);
+
+b.addEventListener('click', () => {
+    rem.classList.remove('test03');
+})
 
