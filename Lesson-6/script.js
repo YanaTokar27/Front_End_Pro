61492ef753d76893d23ffbbe85bbb5ef8ca64b6b
yanatokar <yanochka.tokar22@gmail.com>
2024-01-02 18:51:34 +0200

    Lesson-6

--- Lesson-6/script.js
+++ Lesson-6/script.js
@@ -1,100 +1,110 @@
-let arr = ['1', '2', '3', '4', '5', '6'];
+const task1 = () => {
+    let numbers = [1, 2, 3, 4, 5, 6];
 
-let arr2 = arr.filter((element) => {
-    return element % 2 === 0;
-})
-
-console.log(arr2);
-
-for (let i = 0; i < arr.length; i++) {
-    console.log(i, arr[i]);
-}
-
-let filterFun = (element) => {
-    return element % 2 === 1;
-}
-
-let arr3 = arr.filter(filterFun);
-
-console.log(arr3);
-
-let filterFun2 = (element) => {
-    return element % 2 === 0;
-}
-
-let arr4 = arr.filter(filterFun2);
-
-console.log(arr4);
-
-let user = {
-    name: 'Катя',
-    age: 42,
-    isUser: true,
-    'super User': 'Яна',
-}
-
-let a = user['super User'];
-
-
-for (let key in user) {           //ця конструкція заміняє рядки 43-53
-    console.log('property:', key, 'value:', user[key]);
-}
-
-// let propArray = Object.keys(user);
-// console.log(propArray);
-
-// propArray.forEach((element) => {
-//     console.log(user[element]);
-// })
-
-// for (let i = 0; i < propArray.length; i++) {
-//     let prop = propArray[i];
-//     console.log(user[prop]);
-// }
-
-
-let user1 = {
-    sayHi: function () {
-        console.log('Hi!')
-    },
-    sayHello: () => {
-        alert('Hello!')
+    {
+        console.log("Even numbers:");
+        const evenNumbers = numbers.filter((num) => num % 2 === 0);
+        console.log(evenNumbers);
     }
+
+    console.log("Numbers:");
+    for (let i = 0; i < numbers.length; i++) {
+        console.log("Index:", i, "Value:", numbers[i]);
+    }
+
+    console.log("Odd numbers:");
+    const isOdd = (num) => num % 2 === 1;
+    let oddNumbers = numbers.filter(isOdd);
+    console.log(oddNumbers);
+
+    console.log("Even numbers");
+    const isEven = (num) => num % 2 === 0;
+    let evenNumbers = numbers.filter(isEven);
+    console.log(evenNumbers);
 };
 
-user1.sayHi();
-user1.sayHello();
-
-
-let user2 = {
-    name: 'Олена',
-    age: 35,
-    isUser: true,
-    'super User': 'Інна',
-    sayHi: function () {
-        console.log('Привіт', this.name, this.age);
+const task2 = () => {
+    console.log("=========================================");
+    let user = {
+        name: 'Катя',
+        age: 42,
+        isUser: true,
+        'super User': 'Яна',
     }
-}
 
-user2.sayHi();
+    let a = user['super User'];
 
-let user3 = {
-    name: 'Антон',
-    friendsNames: [],
-    myName: function () {
-        console.log('My name is', this.name);
-    },
-    addFriendName: function (nameNew) {
-        this.friendsNames.push(nameNew);
-    },
-    printFriendsNames: function () {
-        console.log(this.friendsNames);
+
+    for (let key in user) {           //ця конструкція заміняє рядки 42-52
+        console.log('property:', key, 'value:', user[key]);
     }
+
+    // let propArray = Object.keys(user);
+    // console.log(propArray);
+
+    // propArray.forEach((element) => {
+    //     console.log(user[element]);
+    // })
+
+    // for (let i = 0; i < propArray.length; i++) {
+    //     let prop = propArray[i];
+    //     console.log(user[prop]);
+    // }
 };
 
-user3.myName();
-user3.addFriendName('Anna');
-user3.printFriendsNames();
-console.log(user3);
+const task3 = () => {
+    let user1 = {
+        sayHi: function () {
+            console.log('Hi!')
+        },
+        sayHello: () => {
+            alert('Hello!')
+        }
+    };
+
+    user1.sayHi();
+    user1.sayHello();
+};
+
+const task4 = () => {
+    let user2 = {
+        name: 'Олена',
+        age: 35,
+        isUser: true,
+        'super User': 'Інна',
+        sayHi: function () {
+            console.log('Привіт', this.name, this.age);
+        }
+    }
+
+    user2.sayHi();
+};
+
+const task5 = () => {
+    let person = {
+        name: 'Антон',
+        friendsNames: [],
+        // myName: function () {
+        //     console.log('My name is', this.name);
+        // },
+        addFriendName: function (nameNew) {
+            person.friendsNames.push(nameNew);
+        },
+        printFriendsNames: function () {
+            console.log("У " + person.name + " є друзі:");
+            console.log(person.friendsNames);
+        }
+    };
+
+    person.printFriendsNames();
+    person.addFriendName('Olga');
+    person.printFriendsNames();
+};
+
+// task1();
+// task2();
+// task3();
+// task4();
+task5();
 
 
