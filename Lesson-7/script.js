ecdd6eb907650d341ea9a9c7a6965710697efb44
yanatokar <yanochka.tokar22@gmail.com>
2024-01-03 17:55:22 +0200

    Lesson-7

--- Lesson-7/script.js
+++ Lesson-7/script.js
@@ -7,24 +7,22 @@
 let elementById = document.getElementById('some ID');
 console.log('Name ID - ', elementById);
 
-
-
 let divElement = elementById.getElementsByTagName('div')[0];
 console.log('Name Tag - ', divElement);
 
 let elementByClass = elementById.getElementsByClassName('second')[0];
 console.log('Name Class - ', elementByClass);
 
-let elementBySelector = elementByClass.querySelectorAll('.newClass');
-console.log(elementBySelector);
+let elementBySelector = elementByClass.querySelectorAll('.newClass')[0];
+console.log('Selector name - ', elementBySelector);
+
+let firstElementBySelector = elementBySelector.getElementsByTagName('div')[0];
+console.log('First element on Selector - ', firstElementBySelector);
+
+let createDivElement = document.createElement('div');
+console.log('Створений дів - ', createDivElement);
 
 
-
-
-// getElement();
-// task2();
-// task3();
-// task4();
-// task5();
-
-
+let body = document.body;
+body.append('Новий елемент');
+body.appendChild(createDivElement);
